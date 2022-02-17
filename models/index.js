88724dcc31d359004f0a9e38475519c015270c54
Syahrulloh <syahrulloh3@gmail.com>
2022-02-17 11:30:14 +0700

    disable sequelize logging

--- models/index.js
+++ models/index.js
@@ -2,7 +2,8 @@ const config = require('../config/config');
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
-    dialect: config.dialect
+    dialect: config.dialect,
+    logging: false,
 });
 const db = {};
 db.Sequelize = Sequelize;
